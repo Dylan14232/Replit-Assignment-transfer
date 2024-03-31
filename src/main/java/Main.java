aad0a8f90eed5ebb99fc7ed7cdaf575a3c357aa4
Dylan Deavers <165568889+Dylan14232@users.noreply.github.com>
2024-03-31 21:43:44 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,111 @@
+
+// Exercise 1 A:
+  public class Main {
+    public static void main(String[] args) { 
+      int h = 5; 
+      int s = 6; 
+      int sum = h + s; 
+      System.out.println(" > " + "The sum of the two variables is" + "  " + sum);
+ //   }
+ // } 
+
+// Exercise 1 B:
+  // public class Main {
+    // public static void main(String[] args) {
+      int x = 10; 
+      int y = 30; 
+      double z = 30.99;
+      double total = x + y + z;
+      System.out.println(" > " + "The total of the three variables is " + total); 
+ //   }
+ // } 
+//  1 B notes: the exercise starts using Standard java syntax. Int's are assigned to x and y given the variables 10, and 30 ending with the break or ";". As required I have added a third variable that is labled as double and assigned to 30.99 also ending with ";". Since the double was added the sum line will also be a double as the print statement will add the sum and produce a double variable for the output. I also added in the print statement the " > ". The " " insert the symbole before the actual print statement to match the output.
+
+
+// Exercise 1 C:
+ // 
+ // public class Main {
+   // public static void main(String[] args) {
+     boolean isJavaFun = true;
+     boolean isFishTasty = false;
+     System.out.println(" > " + isJavaFun);
+     System.out.println("   " + isFishTasty);   
+//   }
+// }
+// Exercise 1 C notes: Using the standard java syntax, I created two boolean variables and assigned them to true or false. To match the desired output I used println statements to get each print statement on a new line. I then used " " to insert the > symbole to true to match the output. I also used " " to add a space before the false statement to make sure false printed directly under true and not > in the true print statement.
+
+// Exercise 1 D: 
+
+
+ // public class Main {
+   // public static void main(String[] args) {
+     char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+     System.out.print(myVar1);
+     System.out.print(myVar2);
+     System.out.print(myVar3);  
+//   }
+// }
+// Exercise 1 D notes: Using the standard java syntax, I created three char variables as instructed in the assignment and assigned them values. I tried several ways to do this but they ended up adding the values and not printing them as a char. I ended up creating three print statements each for each char vairable without the newline command or the space commands. The result for the code was ABC.
+
+// Exercise 1 E:
+
+  // public class Main {
+    // public static void main(String[] args) {
+      int high = 44;
+          int low = 32;
+          if (high > low) 
+              System.out.println(" > " + "Int high is true.");
+           else 
+              System.out.println(" > " + "Error.");   
+ //       }
+ //     }
+
+// for ecercise 1 E, im not sure if i coded it correctly as i only coded the print statement for true to be outputted or an error to be outputted, however it did give me the desired output. I was trying to use an interactive if elif else statement to add to the user interface to make the code more user firendly however i was not able to get the code to work that way.
+    
+
+// Exercise 1 F: 
+ 
+   // public class Main {
+     // public static void main(String[] args) {
+       var first = "Dylan";
+       var last = "Deavers";
+       var full = first  + " " + last;
+       System.out.println(" > " + full);
+     // }
+   // } 
+// for exercise 1 f i tried using the let keyword, and i tried creating the strings without the keywords as well (ex first_name = "Dylan"). Only var would allow my code to run. if i tried the keyword let, it would not run. I assigned var to first, last and full, on the full line i used quotations for a space and to insert > before my name. I also used + to add the two trings togeather to get my full name on one print line.
+
+
+// Exercise 1 G:
+   // public class Main {
+     // public static void main(String[] args) {
+       String a = "90";
+       int b = 40;
+       String c = a + b;
+       System.out.println(" > " + c);
+  //   }
+  // } 
+// Exercise 1 G notes: As followed in the assignment, i created a string var and assigned it to 90. I then created an int var and assigned it to 40, then i created a string var and assigned it to the sum of the two var's. Lastly i used the print statement to print the result. The reult was 9040.
+
+
+// Exercise 1 H:
+  // public class Main {
+    // public static void main(String[] args) {
+      String statement = (" > " + "That \"dog\" just meeowed!");
+      System.out.println(statement);   
+    
+
+   
+// Exercise 1 H notes: I created a string var and assigned it to the statement line. I used the \"\" to add quotations to the word dog in the sentence. I used the print line to print the statement to get the desired output for the exercise.
+
+// Exercise 1 I:
+   // public class Main {
+     // public static void main(String[] args) {
+       int t = 22;
+       int q = 10;
+       if (t > q);
+           System.out.print(" > " + "t is greater than q");
+     }
+   }
+
+// Exercise 1 I notes: I am not sure if i coded this one correctly. I tried using and if elif else statement to add to the user interface to make the code more user firendly. I have the if statement in there but when i tired using else or elif it would say they were not attached to the if statement i have above. I got the if statement to work and as far as i know if the valuse comes out greater than x the code does not print. I am not sure if i coded it correctly or if i did it right.
